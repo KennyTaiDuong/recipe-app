c7b24c77b6249c55a8cb4216422a9a7c1aa26a7b
KennyTaiDuong <kennyduong537@gmail.com>
2023-12-14 14:23:23 -0600

    styled header to be responsive

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
@@ -8,6 +8,11 @@ const HeaderContainer = styled.div`
   justify-content: space-between;
   align-items: center;
   border-bottom: 2px solid black;
+  flex-direction: column;
+  gap: 1rem;
+  @media screen and (min-width: 520px) {
+    flex-direction: row;
+  }
 `
 
 const Logo = styled(NavLink)`
@@ -21,6 +26,7 @@ const Logo = styled(NavLink)`
 const NavbarContainer = styled.div`
   display: flex;
   align-items: center;
+  gap: 1rem;
 `
 
 const StyledNavLink = styled(NavLink)`
@@ -29,14 +35,20 @@ const StyledNavLink = styled(NavLink)`
 `
 
 const StyledForm = styled.form`
-
+  display: flex;
+  width: 100%;
+  justify-content: center;
 `
 
 const SearchBar = styled.input`
-  border: 0;
-  border-radius: 1rem;
+  border: 2px solid black;
   padding: 0.5rem;
-  width: 40%;
+  width: 100%;
+  max-width: 30rem;
+
+  &:focus {
+    outline: none;
+  }
 `
 
 const SearchButton = styled.button`
@@ -44,6 +56,14 @@ const SearchButton = styled.button`
   background-color: black;
   color: white;
   padding: 0.25rem 0.5rem;
+`
+
+const StyledButton = styled.button`
+  border: 0;
+  padding: 0.5rem;
+  background: black;
+  color: white;
+  font-size: 1rem;
 `
 
 export const Header = () => {
@@ -54,25 +74,26 @@ export const Header = () => {
     setInputValue(e.target.value)
   }
 
-  function handleSearchSubmit() {
+  function handleFormSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
     navigate(`/?query=${inputValue}`)
   }
 
   return (
     <HeaderContainer>
       <Logo to={"/"}>YumYum</Logo>
-      <StyledForm>
+      <StyledForm onSubmit={(e) => handleFormSubmit(e)}>
         <SearchBar 
           type="text" 
           placeholder="Search for a recipe"
           onChange={(e) => handleSearchChange(e)}
           value={inputValue}
-          onSubmit={() => handleSearchSubmit()}
         />
         <SearchButton>Search</SearchButton>
       </StyledForm>
       <NavbarContainer>
         <StyledNavLink to={"/"}>Browse</StyledNavLink>
+        <StyledButton>Post Recipe</StyledButton>
       </NavbarContainer>
     </HeaderContainer>
   )
