bc1140ae3da66e209ef49418dd1d19d731344c77
KennyTaiDuong <kennyduong537@gmail.com>
2023-12-14 09:33:30 -0600

    added routes and global styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom"
+
+import { Layout } from "./components/Layout"
+import { Home } from "./pages/Home"
+import { RecipeDetail } from "./pages/RecipeDetail"
+import { createGlobalStyle } from "styled-components";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const GlobalStyle = createGlobalStyle`
+    * {
+      box-sizing: border-box;
+      margin: 0;
+      padding: 0;
+    }
+
+    html,
+    body,
+    :root {
+      font-family: 'Montserrat', sans-serif;
+      scroll-behavior: smooth;
+    }
+    button,
+    a {
+      font-family: 'Montserrat', sans-serif;
+      cursor: pointer;
+    }
+    img{
+      user-select: none;
+    }
+  `;
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Route>
+    )
+  )
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <GlobalStyle />
+      <RouterProvider router={router} />
     </>
   )
 }
---
+++ client/src/pages/RecipeDetail.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const RecipeContainer = styled.div`
+
+`
+
+export const RecipeDetail = () => {
+  return (
+    <RecipeContainer>
+      Recipe
+    </RecipeContainer>
+  )
+}
